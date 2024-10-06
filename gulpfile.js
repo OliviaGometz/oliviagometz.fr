ad7c667fb838a416fd73034bd10dea21315bc56d
Olivia Gometz <oliviagometz@gmail.com>
2024-10-06 18:38:30 +0200

    chore: passage à dart-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp"),
-  sass = require("gulp-sass")(require("sass")),
+  sass = require("gulp-dart-sass"),
   postcss = require("gulp-postcss"),
   autoprefixer = require("autoprefixer"),
   cssnano = require("cssnano"),
@@ -20,8 +20,7 @@ const paths = {
 function style() {
   return gulp
     .src(paths.styles.src)
-    .pipe(sass())
-    .on("error", sass.logError)
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(gulp.dest(paths.styles.dest));
 }
