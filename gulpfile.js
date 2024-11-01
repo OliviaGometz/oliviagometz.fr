dec22f3839f8f3ab08b283fdf00c2e9d952f4d84
Olivia Gometz <oliviagometz@gmail.com>
2024-11-01 20:40:24 +0100

    feat: page dédicaces

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,49 @@ const paths = {
   },
 };
 
+const frenchMonths = {
+  "01": "janvier",
+  "02": "février",
+  "03": "mars",
+  "04": "avril",
+  "05": "mai",
+  "06": "juin",
+  "07": "juillet",
+  "08": "août",
+  "09": "septembre",
+  "10": "octobre",
+  "11": "novembre",
+  "12": "décembre"
+};
+
+function dateFormat(input, returnMonth = true, returnYear = true) {
+  var date = input.split('-');
+  var day = date[2] ? date[2] + ' ' : '';
+  var month = returnMonth ? frenchMonths[date[1]] : '';
+  var year = returnYear ? ' ' + date[0] : '';
+
+  return day + month + year;
+}
+
+function isSameMonth(input1, input2) {
+  var month1 = input1.split('-')[1];
+  var month2 = input2.split('-')[1];
+
+  return month1 == month2;
+}
+
+function datesToString(input) {
+  var formattedDate = '';
+
+  if (input.start != input.end) {
+    formattedDate = 'Du ' + dateFormat(input.start, !isSameMonth(input.start, input.end), false) + ' au ' + dateFormat(input.end);
+  } else {
+    formattedDate = 'Le ' + dateFormat(input.start);
+  }
+
+  return formattedDate;
+}
+
 function style() {
   return gulp
     .src(paths.styles.src)
@@ -30,17 +73,24 @@ function html() {
     .src("app/pages/**/*.njk")
     .pipe(
       data(function () {
+        const books = require("./app/content/books.json");
+        const events = require("./app/content/events.json");
         const linktree = require("./app/content/linktree.json");
         const logos = require("./app/content/logos.json");
+        const places = require("./app/content/places.json");
         const podcasts = require("./app/content/podcasts.json");
         const timeline = require("./app/content/timeline.json");
         const websites = require("./app/content/websites.json");
-        return { linktree, logos, podcasts, timeline, websites };
+        return { books, events, linktree, logos, places, podcasts, timeline, websites };
       })
     )
     .pipe(
       nunjucksRender({
         path: ["app/templates"],
+        manageEnv: function (nunjucksEnv) {
+          nunjucksEnv.addFilter("dateFormat", dateFormat);
+          nunjucksEnv.addFilter("datesToString", datesToString);
+        },
       })
     )
     .pipe(gulp.dest(paths.njk.dest));
---
+++ js/events.js
@@ -0,0 +1,48 @@
+const cssHidden = 'js-hidden';
+
+// Afficher tous les contenus masqués si l'url se termine par ?private
+
+if (window.location.href.endsWith('?private')) {
+    document.querySelectorAll('.' + cssHidden).forEach((element) => {
+        element.classList.remove(cssHidden);
+    });
+}
+
+// Compléter le décompte + appliquer styles appropriés aux événements
+
+const today = new Date();
+
+const dayDiff = function(date) {
+    return Math.ceil((date.getTime() - today.getTime()) / (1000 * 3600 * 24));
+};
+
+const displayDayDiff = function(text, block) {
+    block.querySelectorAll('.event__decompte')[0].textContent = text;
+};
+
+const emphasisImminente = function(block) {
+    block.classList.add("card--white--emphasis");
+    block.querySelectorAll('details')[0].setAttribute("open", true);
+};
+
+document.querySelectorAll('[data-js="event"]').forEach((event) => {
+    const eventStart = new Date(event.dataset.eventstart);
+    const eventEnd = new Date(event.dataset.eventend);
+
+    if (dayDiff(eventEnd) < 0) {
+        event.classList.add(cssHidden);
+        displayDayDiff('Terminé', event);
+    } else if (dayDiff(eventStart) < 1) {
+        emphasisImminente(event);
+        displayDayDiff('En ce moment', event);
+    } else if (dayDiff(eventStart) == 1) {
+        emphasisImminente(event);
+        displayDayDiff('Demain', event);
+    } else if (dayDiff(eventStart) < 11) {
+        displayDayDiff('Dans ' + dayDiff(eventStart) + ' jours', event);
+    } else if (dayDiff(eventStart) < 28) {
+        displayDayDiff('Dans ' + Math.round(dayDiff(eventStart)/7) + ' semaines', event);
+    } else {
+        displayDayDiff('Dans ' + Math.round(dayDiff(eventStart)/30) + ' mois', event);
+    }
+});
